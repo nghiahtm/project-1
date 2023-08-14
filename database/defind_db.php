5ab51ff62241aec5b2cc14f9c62833436561d971
Nghia <nghia.htm@gmail,com>
2023-08-14 17:53:49 +0700

    change state order

--- database/defind_db.php
+++ database/defind_db.php
@@ -2,6 +2,6 @@
 const servername = '127.0.0.1';
 const username = 'root';
 const password ='admin123';
-const db ='Project1';
+const db ='project3';
 
 // isset hàm kiểm tra sự tồn tại của một đối tượng
